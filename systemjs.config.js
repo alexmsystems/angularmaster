7957b302e27ee5f2278beb44917b54b0004e0aff
Aleksei Morin <aleksei_morin@epam.com>
2016-12-14 20:44:12 +0300

    1

--- systemjs.config.js
+++ systemjs.config.js
@@ -14,7 +14,7 @@
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             'rxjs': 'npm:rxjs',
-            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+            'angularmaster': 'npm:angularmaster',
         },
         packages: {
             app: {
