803a443bba798443e91ce3dbf59289df18511133
Aleksei Morin <aleksei_morin@epam.com>
2016-12-14 20:47:38 +0300

    2

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,10 +1,14 @@
 ﻿(function (global) {
     System.config({
         paths: {
+            // paths serve as alias
             'npm:': 'node_modules/'
         },
+        // map tells the System loader where to look for things
         map: {
-            app: 'app',
+            // our app is within the app folder
+            app : 'ScriptsApp', // 'dist',
+            // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
             '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
@@ -13,21 +17,13 @@
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+            // other libraries
             'rxjs': 'npm:rxjs',
-            'angularmaster': 'npm:angularmaster',
         },
+        // packages tells the System loader how to load when no filename and/or no extension
         packages: {
-            app: {
-                main: './main.js',
-                defaultExtension: 'js'
-            },
-            rxjs: {
-                defaultExtension: 'js'
-            },
-            'angular2-in-memory-web-api': {
-                main: './index.js',
-                defaultExtension: 'js'
-            }
+            app: { main: './boot.js', defaultExtension: 'js' },
+            rxjs: { defaultExtension: 'js' }
         }
     });
 })(this);
